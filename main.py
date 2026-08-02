4d88b8c9787ed003b3ea1bf9412be4f452ed2c98
Pat Thompson <229507349+PatThompson1306@users.noreply.github.com>
2026-08-02 13:43:56 +0100

    Conditional statements in Jinja2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI() 
+
+templates = Jinja2Templates(directory="templates")
 
 blog_posts: list[dict] = [
     {
@@ -18,33 +22,18 @@ blog_posts: list[dict] = [
     }
 ]
 
-app = FastAPI() 
+@app.get("/", include_in_schema=False) 
+# @app.get("/posts", include_in_schema=False) 
+def home(request: Request):  
+    return templates.TemplateResponse(request, "home.html", {"posts": blog_posts, "title": "Awesome Page!!!"})
 
-# home route displaying as HTML with the number of blog posts available
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)  
-def home():  
-    return f"<h1>Welcome to this Blog!</h1><p>There are currently {len(blog_posts)} blog posts available.</p>" 
 
-# api/blogposts route to get all blog posts - presented in JSON format if needed for programmatic access
 @app.get("/api/blogposts")
 def get_blog_posts():
     return {"data": blog_posts}
 
-'''
-posts route displaying specific blog post content
-The function creates a variable called html_content that holds an H1 element.
-The function then loops through the content of the blog_posts list and adds subsequent HTML elements for each post.
-'''
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False) 
 
+@app.get("/posts", include_in_schema=False) 
 def posts():
-    html_content = "<h1>Blog Posts</h1>"
-    for post in blog_posts:
-        html_content += f"<h2>{post['title']}</h2>"
-        html_content += f"<p><strong>Author:</strong> {post['author']}</p>"
-        html_content += f"<p><strong>Date Posted:</strong> {post['date_posted']}</p>"
-        html_content += f"<p>{post['content']}</p>"
-        html_content += "<hr>"
-    return html_content
-
+    return blog_posts
 
